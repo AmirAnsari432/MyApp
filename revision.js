568642290cd9be3fbf911cca9ed6fa7e3c372a94
Amir Ansari <amirbhai432@gmail.com>
2022-09-25 12:35:40 +0530

    logInPage

---
+++ revision.js
@@ -0,0 +1,107 @@
+// // const arr = [1, 2, 3, -2, -8];
+
+// // arr.forEach((ele, index) => {
+// //   console.log(ele, index);
+// // });
+
+// const names = [
+//   { name: "Manoj", age: 27 },
+//   { name: "Praneeth", age: 24 },
+//   { name: "Sudheer", age: 42 },
+//   { name: "Shaik", age: 31 },
+//   { name: "tom", age: 23 },
+//   { name: "supriya", age: 25 },
+//   { name: "amir", age: 22 },
+// ];
+
+// // let count = 0;
+// // names.forEach(person=>{
+// //     if(person.age>20)
+// //         count++;
+// // });
+// // console.log(count);
+
+//  names.forEach((person) => {
+//   if (person.age > 20 && person.age < 30) {
+//     return person.name;
+    
+//   }
+// });
+
+// const string = ["a", "b", "c", "d", "a", "b", "e", "f"];
+
+// const occurances = {};
+
+// string.forEach((char) => {
+//   if (occurances[char] === undefined || occurances[char] === null) {
+//     occurances[char] = 1;
+//   } else {
+//     occurances[char] = occurances[char] + 1;
+//   }
+// });
+
+// console.log(occurances);,
+
+
+
+// const num = [2,3,4,5,,6]
+
+
+// num.forEach((e) => {
+//   console.log(e*e)
+// })
+
+
+// const str = 'Amir '
+
+// arr = Array.from(str)
+// console.log(arr)
+
+// const number = [2,3,4,5,6]
+
+// // for (let i in number )
+// // {
+// //   console.log(i)
+// // }
+
+// for (let i in number ){
+//   console.log(i)
+// }
+
+// unsift 
+// var list  = ['green','blue', 'orange'] ;
+
+// list.unshift('yellow', 'purple')
+// list.pop();
+// console.log(list);
+
+// Boolen
+
+// const var1 = null ;
+// const var2 = 1 ;
+// console.log(Boolean(var1))
+// console.log(!!var1) 
+// console.log(Boolean(var2))
+// console.log((!!var2))
+
+// sort method d
+
+// var array1 = [50,25,20]
+// console.log(array1.sort(function(a,b){
+//     return a-b
+// }))
+// console.log(array1.sort(function(a,b){
+//     return b-a
+// }))
+
+// out put  
+
+// (3) [20, 25, 50]
+// (3) [50, 25, 20]
+
+
+// var apple = true +  true * 3 
+// console.log(apple)
+
+// output = 4 
+
